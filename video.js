63dc560c6b1f05ac553963b99cfeb01247fbadc4
Jefferson000 <jefersontorres000@gmail.com>
2021-11-25 11:08:01 -0600

    tensorflow.js

--- video.js
+++ video.js
@@ -4,19 +4,14 @@ let poses = [];
 let started = false;
 var audio = document.getElementById("audioElement");
 
-
-
-// p5.js setup() function to set up the canvas for the web cam video stream
 function setup() {
-  //creating a canvas by giving the dimensions
-  const canvas = createCanvas(400, 450);
+  const canvas = createCanvas(500, 500);
   canvas.parent("video");
 
   videofeed = createCapture(VIDEO);
   videofeed.size(width, height);
   console.log("setup");
 
-  // setting up the poseNet model to feed in the video feed.
   posenet = ml5.poseNet(videofeed);
 
   posenet.on("pose", function (results) {
@@ -27,7 +22,6 @@ function setup() {
   noLoop();
 }
 
-// p5.js draw function() is called after the setup function
 function draw() {
   if (started) {
     image(videofeed, 0, 0, width, height);
@@ -35,7 +29,6 @@ function draw() {
   }
 }
 
-// toggle button for starting the video feed
 function start() {
   select("#startstop").html("stop");
   document.getElementById("startstop").addEventListener("click", stop);
@@ -43,7 +36,6 @@ function start() {
   loop();
 }
 
-// toggle button for ending the video feed
 function stop() {
   select("#startstop").html("start");
   document.getElementById("startstop").addEventListener("click", start);
@@ -52,13 +44,11 @@ function stop() {
   noLoop();
 }
 
-// defining the parameters used for the posenet : the tracking of the eyes
 var rightEye,
   leftEye,
   defaultRightEyePosition = [],
   defaultLeftEyePosition = [];
 
-//function to calculate the position of the various keypoints
 function calEyes() {
   for (let i = 0; i < poses.length; i++) {
     let pose = poses[i].pose;
@@ -67,7 +57,6 @@ function calEyes() {
       rightEye = pose.keypoints[2].position;
       leftEye = pose.keypoints[1].position;
 
-      // keypoints are the points representing the different joints on the body recognized by posenet
 
       while (defaultRightEyePosition.length < 1) {
         defaultRightEyePosition.push(rightEye.y);
@@ -77,7 +66,6 @@ function calEyes() {
         defaultLeftEyePosition.push(leftEye.y);
       }
 
-      // if the current position of the body is too far from the original position blur function is called
       if (Math.abs(rightEye.y - defaultRightEyePosition[0]) > 20) {
         blur();
       }
@@ -88,7 +76,6 @@ function calEyes() {
   }
 }
 
-//function to blur the background and add audio effect
 function blur() {
   document.body.style.filter = "blur(5px)";
   document.body.style.transition = "1s";
@@ -97,7 +84,6 @@ function blur() {
   audio.play();
 }
 
-//function to remove the blur effect
 function removeblur() {
   document.body.style.filter = "blur(0px)";
   var audio = document.getElementById("audioElement");
